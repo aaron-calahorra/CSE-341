feaf20acc451f34a3121f5631329b9e880811908
aaron-calahorra <cal21082@byui.edu>
2022-09-14 20:20:30 -0600

    Test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Aaron Calahorra')
+  res.send('Hello, Aaron Calahorra')
 })
 
 app.listen(port, () => {
