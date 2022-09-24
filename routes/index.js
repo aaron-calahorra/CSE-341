49f1fb0b4ac1db660127b1ea920205d3e1c37863
aaron-calahorra <cal21082@byui.edu>
2022-09-24 09:09:33 -0600

    L04 Personal Assignment

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,21 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-router.use('/contacts', require('./contacts'))
+// router.use('/contacts', require('./contacts'))
+
+// module.exports = router;
+
+const router = require('express').Router();
+const contact = require('./contacts.js');
+
+router.use('/', require('./swagger'));
+router.use('/contacts', contact);
+router.use(
+  '/',
+  (docData = (req, res) => {
+    let docData = "Hello world!";
+    res.send(docData);
+  })
+);
 
 module.exports = router;
